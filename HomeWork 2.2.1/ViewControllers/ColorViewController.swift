ff8cc625e1250fc6806ca98937272908df704e42
Селбаев Алмас <almasselbayev@gmail.com>
2022-03-20 22:55:42 +0600

    prepare Segue

--- HomeWork 2.2.1/ViewControllers/ColorViewController.swift
+++ HomeWork 2.2.1/ViewControllers/ColorViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class ColorViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navigationVC = segue.destination as? UINavigationController else { return }
+        guard let settingsVC = navigationVC.topViewController as? SettingsViewController else { return }
         
+        settingsVC.rgb = getRGB(from: view.backgroundColor)
+    }
+    
+    private func getRGB(from view: UIColor?) -> UIColor {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        view?.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return UIColor(
+                red: CGFloat(red),
+                green: CGFloat(green),
+                blue: CGFloat(blue),
+                alpha: CGFloat(alpha))
     }
     
 }
+
--- HomeWork 2.2.1/ViewControllers/SettingsViewController.swift
+++ HomeWork 2.2.1/ViewControllers/SettingsViewController.swift
@@ -23,12 +23,14 @@ class SettingsViewController: UIViewController {
     @IBOutlet var greenTextField: UITextField!
     @IBOutlet var blueTextField: UITextField!
     
+    var rgb: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         colorView.layer.cornerRadius = 10
+        updateColorInfo(color: rgb)
         
-        setColor()
     }
     
     
@@ -38,10 +40,13 @@ class SettingsViewController: UIViewController {
         switch sender {
         case redSlider:
             redLabel.text = string(redSlider.value)
+            redTextField.text = string(redSlider.value)
         case greenSlider:
             greenLabel.text = string(greenSlider.value)
+            greenTextField.text = string(greenSlider.value)
         default:
             blueLabel.text = string(blueSlider.value)
+            blueTextField.text = string(blueSlider.value)
         }
     }
     
@@ -61,5 +66,23 @@ class SettingsViewController: UIViewController {
             alpha: 1.0)
     }
     
+    private func updateColorInfo(color: UIColor) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        redLabel.text = string(Float(red))
+        greenLabel.text = string(Float(green))
+        blueLabel.text = string(Float(blue))
+        
+        redTextField.text = string(Float(red))
+        greenTextField.text = string(Float(green))
+        blueTextField.text = string(Float(blue))
+        
+        colorView.backgroundColor = color
+    }
 }
 
